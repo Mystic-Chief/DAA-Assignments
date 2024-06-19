3b89c49877ea857b1aa87f7a3f4218a225de183c
Mystic-Chief <107339137+Mystic-Chief@users.noreply.github.com>
2024-06-19 23:32:21 +0530

    Create Question_1.py

---
+++ Question_1.py
@@ -0,0 +1,16 @@
+def check_number(num):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+    
+    if num > 0:
+        print(f"{num} is positive")
+    elif num < 0:
+        print(f"{num} is negative")
+    else:
+        print(f"{num} is zero")
+
+check_number(9)
+check_number(-22)
+check_number(0)
